991ddcbab68a32eeea869da69727f18f19c490d3
Lucas Saliés Brum <sistematico@gmail.com>
2023-12-25 16:46:34 -0400

    Update

--- src/utils/yt.ts
+++ src/utils/yt.ts
@@ -8,40 +8,48 @@ export async function getInfo(url: string) {
   return metadata.title
 }
 
-// import * as ytdlp from 'yt-dlp-wrap';
-
 export async function downloadVideo(url: string): Promise<string | null> {
-  const metadata = await ytDlpWrap.getVideoInfo(url);
-  if (!metadata.title) return null;
+  try {
+    const metadata = await ytDlpWrap.getVideoInfo(url)
+    if (!metadata.title) return null
 
-  const filename = metadata.title + '.mp4';
-  const readableStream = ytDlpWrap.execStream([url, '-f', 'best[ext=mp4]']);
+    const filename = metadata.title + '.mp4'
+    const readableStream = ytDlpWrap.execStream([url, '-f', 'best[ext=mp4]'])
 
-  return new Promise((resolve, reject) => {
-    const chunks: Buffer[] = [];
+    const chunks: Buffer[] = []
+    return new Promise((resolve, reject) => {
+      readableStream.on('data', chunk => chunks.push(chunk))
 
-    readableStream.on('data', (chunk) => {
-      chunks.push(chunk);
-    });
+      readableStream.on('end', async () => {
+        try {
+          await Bun.write(filename, Buffer.concat(chunks)) // Download concluído.
+          resolve(filename)
+        } catch (err) {
+          console.error('Erro ao salvar o vídeo:', err)
+          reject(null)
+        }
+      })
 
-    readableStream.on('end', async () => {
-      try {
-        await Bun.write(filename, Buffer.concat(chunks));
-        console.log('Download concluído.');
-        resolve(filename);
-      } catch (err) {
-        console.error('Erro ao salvar o vídeo:', err);
-        reject(null);
-      }
-    });
+      readableStream.on('error', err => {
+        console.error('Erro ao baixar o vídeo:', err)
+        reject(null)
+      })
 
-    readableStream.on('error', (err) => {
-      console.error('Erro ao baixar o vídeo:', err);
-      reject(null);
-    });
-  });
+      // Configurar um tempo limite para o download
+      const timeout = 60000 // 60 segundos, ajuste conforme necessário
+      setTimeout(() => {
+        if (chunks.length === 0) {
+          // Se nenhum dado foi recebido
+          console.error('Download excedeu o tempo limite.')
+          reject(null)
+        }
+      }, timeout)
+    })
+  } catch (error) {
+    console.error('Erro durante o download:', error)
+    return null
+  }
 }
-
 
 function validDomain(url: string): boolean {
   try {
