9d1ea33f7d09b0b99b815a81d0804cb3d3de64ff
Lucas Saliés Brum <sistematico@gmail.com>
2023-12-25 22:27:07 -0400

    Update

--- src/mw/flood.ts
+++ src/mw/flood.ts
@@ -1,45 +1,44 @@
-import { Context, MiddlewareFn, NextFunction } from 'grammy';
+import { Context, MiddlewareFn, NextFunction } from 'grammy'
 
-// Definição da interface para cada item na fila
+
 interface QueueItem {
-  ctx: Context;
-  userId: number;
+  ctx: Context
+  userId: number
 }
 
-// Configurações do plugin
-const FLOOD_TIME_LIMIT = 5000; // Tempo em milissegundos para o controle de flood
+const FLOOD_TIME_LIMIT = Number(Bun.env.FLOOD_TIME_LIMIT) || 5000
 
 // Criação do plugin de controle de flood
 export function floodControlPlugin(): MiddlewareFn {
-  const messageQueue: QueueItem[] = [];
-  let isProcessing = false;
-  const userLastMessageTime: Record<number, number> = {};
+  let isProcessing = false
+  
+  const messageQueue: QueueItem[] = []
+  const userLastMessageTime: Record<number, number> = {}
 
-  // Função para processar a fila
   const processQueue = async (next: NextFunction) => {
-    if (isProcessing || messageQueue.length === 0) return;
-    isProcessing = true;
+    if (isProcessing || messageQueue.length === 0) return
+    isProcessing = true
 
-    const { ctx, userId } = messageQueue.shift()!;
-    const currentTime = Date.now();
+    const { ctx, userId } = messageQueue.shift()!
+    const currentTime = Date.now()
 
     if (userLastMessageTime[userId] && (currentTime - userLastMessageTime[userId]) < FLOOD_TIME_LIMIT) {
-      await ctx.reply('Por favor, aguarde um momento antes de enviar outra mensagem.');
+      await ctx.reply('Por favor, aguarde um momento antes de enviar outra mensagem.')
     } else {
       userLastMessageTime[userId] = currentTime;
-      await next(); // Processa a mensagem
+      await next()
     }
 
-    isProcessing = false;
-    setTimeout(() => processQueue(next), 100);
-  };
+    isProcessing = false
+    setTimeout(() => processQueue(next), 500)
+  }
 
-  // Middleware do plugin
   return async (ctx, next) => {
     if (ctx.from) {
       const userId = ctx.from.id
       messageQueue.push({ ctx, userId })
     }
+
     await processQueue(next)
-  };
+  }
 }
